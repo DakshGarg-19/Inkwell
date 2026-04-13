600a5d4e410a3d073dc2705e56d61f744e8b9347
Daksh Garg <dakshgarg64@gmail.com>
2026-04-13 17:33:30 +0530

    chore: remove unused dependencies (@tanstack/react-query, axios) and update project name

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
-import axios from 'axios';
 import { useNavigate, Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { PenLine } from 'lucide-react';
